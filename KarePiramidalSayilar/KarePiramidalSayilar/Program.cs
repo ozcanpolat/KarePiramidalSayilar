03dfb1d0f6f154069f45b37dc6d6e53011a961fc
ozcanpolat <93453642+ozcanpolat@users.noreply.github.com>
2023-07-11 16:01:08 +0300

    KarePiramidalSayilar

---
+++ KarePiramidalSayilar/KarePiramidalSayilar/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace KarePiramidalSayilar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int toplam = 0;
+            Console.Write("N : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                toplam = 0;
+                for (int j = 1; j <= i; j++)
+                {
+                    toplam += j * j;
+                    Console.Write(j + "^2");
+                    if (j != i)
+                    {
+                        Console.Write(" + ");
+                    }
+                    else
+                    {
+                        Console.Write(" = ");
+                    }
+                }
+                Console.Write(toplam);
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
